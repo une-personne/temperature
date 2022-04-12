36946eaab3cb022dbb4115e95eb01ffd927d3a26
une-personne <103508249+une-personne@users.noreply.github.com>
2022-04-12 14:36:40 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,16 @@
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.clearScreen()
+        basic.showString("T=")
+        basic.showNumber(input.temperature())
+        basic.showString("DEGRES")
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+})
